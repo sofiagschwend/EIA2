edab3bb566d6ad1876f17087022dec5f4d251f1b
Sofia <Sofia@DESKTOP-MS98UH4.hfu-open.fu.hs-furtwangen.de>
2018-06-16 16:40:02 +0200

    Aufgabe9 Unterwasserwelt

--- Aufgabe9/Canvas.js
+++ Aufgabe9/Canvas.js
@@ -6,53 +6,59 @@ var Aufgabe9_Canvas;
         let canvas = document.getElementsByTagName("canvas")[0];
         crc2 = canvas.getContext("2d");
         console.log(crc2);
+        // Hintergrund Einf�rben
         crc2.fillStyle = "lightblue";
         crc2.fillRect(0, 0, 1000, 700);
-        /*EIN FISCH
+        // Funktionsaufrufe - wer zuerst kommt, malt zuerst
+        drawRocks(200, 550);
+        drawRocks(350, 550);
+        drawGrass(925, 150);
+        drawSand();
+        //drawFish Aufgruf �ber for-Schleife
+        for (let i = 0; i < 11; i++) {
+            let x = Math.random() * crc2.canvas.width;
+            let y = Math.random() * crc2.canvas.height;
+            drawFish(x, y);
+        }
+        drawBubbles(150, 150, 30, 60, 3);
+        drawBubbles(220, 40, 80, 160, 5);
+        drawBubbles(60, 220, 80, 260, 8);
+        drawBox(620, 600);
+    } // init funktion zu ***********************************************
+    // FELSEN
+    function drawRocks(_x, _y) {
         crc2.beginPath();
-        crc2.moveTo(300, 150);
-        crc2.quadraticCurveTo(330, 115, 380, 170);
-        crc2.lineTo(380, 140);
-        crc2.quadraticCurveTo(330, 185, 300, 150);
-        crc2.stroke();*/
-        // I BIMS 1 SEEGRAS        
+        crc2.moveTo(_x, _y);
+        crc2.lineTo(_x + 125, _y - 50);
+        crc2.lineTo(_x + 150, _y);
+        crc2.lineTo(_x + 300, _y + 50);
+        crc2.lineTo(_x, _y + 150);
+        crc2.lineTo(_x, _y);
+        crc2.closePath();
+        crc2.fillStyle = "gray";
+        crc2.strokeStyle = "gray";
+        crc2.stroke();
+        crc2.fill();
+    }
+    // I BIMS 1 SEEGRAS        
+    function drawGrass(_x, _y) {
         crc2.beginPath();
-        crc2.moveTo(925, 150);
-        crc2.quadraticCurveTo(900, 200, 925, 250);
-        crc2.quadraticCurveTo(950, 275, 950, 350);
-        crc2.quadraticCurveTo(925, 400, 960, 450);
-        crc2.lineTo(890, 450);
-        crc2.quadraticCurveTo(900, 400, 920, 350);
-        crc2.quadraticCurveTo(940, 300, 900, 250);
-        crc2.quadraticCurveTo(900, 190, 925, 150);
+        crc2.moveTo(_x, _y);
+        crc2.quadraticCurveTo(_x - 25, _y + 50, 925, 250); //(900, 200, 925, 250);
+        crc2.quadraticCurveTo(_x + 25, _y + 125, 950, 350); //(950, 275, 950, 350);
+        crc2.quadraticCurveTo(_x, _y + 250, 960, 450); //(925, 400, 960, 450);
+        crc2.lineTo(_x - 35, _y + 300); //(890, 450);
+        crc2.quadraticCurveTo(_x - 25, _y + 250, 920, 350); //(900, 400, 920, 350);
+        crc2.quadraticCurveTo(_x + 15, _y + 150, 900, 250); //(940, 300, 900, 250);
+        crc2.quadraticCurveTo(_x - 25, _y + 40, 925, 150); //(900, 190, 925, 150);
         crc2.fillStyle = "green";
+        crc2.strokeStyle = "green";
         crc2.stroke();
         crc2.fill();
         crc2.closePath();
-        // FELSEN 1
-        crc2.beginPath();
-        crc2.moveTo(200, 550);
-        crc2.lineTo(325, 500);
-        crc2.lineTo(500, 700);
-        crc2.lineTo(200, 700);
-        crc2.lineTo(200, 550);
-        crc2.closePath();
-        crc2.fillStyle = "gray";
-        crc2.stroke();
-        crc2.fill();
-        // FELSEN 2
-        crc2.beginPath();
-        crc2.moveTo(400, 550);
-        crc2.lineTo(450, 500);
-        crc2.lineTo(600, 700);
-        crc2.lineTo(200, 700);
-        crc2.lineTo(150, 650);
-        crc2.lineTo(400, 550);
-        crc2.closePath();
-        crc2.fillStyle = "gray";
-        crc2.stroke();
-        crc2.fill();
-        // SAND        
+    }
+    // SAND 
+    function drawSand() {
         crc2.beginPath();
         crc2.moveTo(0, 650);
         crc2.quadraticCurveTo(350, 500, 500, 600);
@@ -61,17 +67,59 @@ var Aufgabe9_Canvas;
         crc2.lineTo(0, 700);
         crc2.lineTo(0, 650);
         crc2.closePath();
-        crc2.fillStyle = "lightyellow";
+        crc2.fillStyle = "lightyellow"; // F�llung
+        crc2.strokeStyle = "lightyellow"; // Linienstyle
         crc2.stroke();
         crc2.fill();
-        // Funktionsaufruf
-        /*drawFish(300, 150);
-
-        for (let i: number = 0; i < 10; i++) {
-            let x: number = Math.random() * crc2.canvas.width;
-            let y: number = Math.random() * crc2.canvas.height;
-            drawFish(x, y);
-        }*/
-    } // init funktion zu
+    }
+    // BUBBLES 
+    function drawBubbles(_x, _y, _w, _h, _a) {
+        for (let i = 0; i < _a; i++) {
+            let width = Math.random() * _w;
+            let height = Math.random() * _h;
+            let scale = Math.random() * 13;
+            crc2.beginPath();
+            // crc2.arc(150, 150, 30, 60, 3); -> wird in init aufgerufen
+            crc2.arc(_x + width, _y + height, scale, 0, 3 * Math.PI);
+            crc2.stroke();
+            crc2.fillStyle = "darkblue"; // F�llung
+            crc2.strokeStyle = "darkblue"; // Linienstyle
+            crc2.fill();
+        }
+    }
+    // FISCHE
+    function drawFish(_x, _y) {
+        crc2.beginPath();
+        crc2.moveTo(_x, _y);
+        crc2.quadraticCurveTo(_x + 30, _y - 35, _x + 80, _y + 20);
+        crc2.lineTo(_x + 80, _y - 10);
+        crc2.quadraticCurveTo(_x + 30, _y + 35, _x, _y);
+        crc2.fillStyle = "orange";
+        crc2.strokeStyle = "orange";
+        crc2.stroke();
+        crc2.fill();
+    }
+    // SCHATZTRUHE
+    function drawBox(_x, _y) {
+        crc2.beginPath(); //Box von der Seite
+        crc2.moveTo(_x, _y);
+        crc2.lineTo(_x + 200, _y);
+        crc2.lineTo(_x + 200, _y - 100);
+        crc2.lineTo(_x, _y - 100);
+        crc2.closePath();
+        crc2.fillStyle = "brown";
+        crc2.strokeStyle = "white";
+        crc2.stroke();
+        crc2.fill();
+        crc2.beginPath(); //Deckel von der Seite
+        crc2.moveTo(_x, _y - 100);
+        crc2.lineTo(_x + 190, _y - 150);
+        crc2.quadraticCurveTo(_x + 70, _y - 190, _x, _y - 100);
+        crc2.closePath();
+        crc2.fillStyle = "brown";
+        crc2.strokeStyle = "brown";
+        crc2.stroke();
+        crc2.fill();
+    }
 })(Aufgabe9_Canvas || (Aufgabe9_Canvas = {})); // namespace zu
 //# sourceMappingURL=Canvas.js.map
--- Aufgabe9/Canvas.ts
+++ Aufgabe9/Canvas.ts
@@ -7,97 +7,139 @@ namespace Aufgabe9_Canvas {
         crc2 = canvas.getContext("2d");
         console.log(crc2);
 
+        // Hintergrund Einfärben
         crc2.fillStyle = "lightblue";
         crc2.fillRect(0, 0, 1000, 700);
 
-        /*EIN FISCH
-        crc2.beginPath();
-        crc2.moveTo(300, 150);
-        crc2.quadraticCurveTo(330, 115, 380, 170);
-        crc2.lineTo(380, 140);
-        crc2.quadraticCurveTo(330, 185, 300, 150);
-        crc2.stroke();*/
-        
+        // Funktionsaufrufe - wer zuerst kommt, malt zuerst
+        drawRocks(200, 550);
+        drawRocks(350, 550);
+
+        drawGrass(925, 150);
+        drawSand();
+
+        //drawFish Aufgruf über for-Schleife
+        for (let i: number = 0; i < 11; i++) { // for Schleife die Koordinaten für Fische festlegt
+          let x: number = Math.random() * crc2.canvas.width;
+          let y: number = Math.random() * crc2.canvas.height;
+          drawFish(x, y);
+        }
+
+
+        drawBubbles(150, 150, 30, 60, 3);
+        drawBubbles(220, 40, 80, 160, 5);
+        drawBubbles(60, 220, 80, 260, 8);
+                
+        drawBox(620, 600);
+            
+
+        }// init funktion zu ***********************************************
+
+        // FELSEN
+        function drawRocks(_x: number, _y: number): void {
+            crc2.beginPath();
+            crc2.moveTo(_x, _y);
+            crc2.lineTo(_x + 125, _y - 50);
+            crc2.lineTo(_x + 150, _y);
+            crc2.lineTo(_x + 300, _y + 50);
+            crc2.lineTo(_x, _y + 150);
+            crc2.lineTo(_x, _y);
+            crc2.closePath();
+            crc2.fillStyle = "gray";
+            crc2.strokeStyle = "gray";
+            crc2.stroke();
+            crc2.fill();
+        }
+
         // I BIMS 1 SEEGRAS        
-        crc2.beginPath();
-        crc2.moveTo(925,150);
-        crc2.quadraticCurveTo(900, 200, 925, 250);
-        crc2.quadraticCurveTo(950, 275, 950, 350);
-        crc2.quadraticCurveTo(925, 400, 960, 450);
-        crc2.lineTo(890, 450);
-        crc2.quadraticCurveTo(900, 400, 920, 350);
-        crc2.quadraticCurveTo(940, 300, 900, 250);
-        crc2.quadraticCurveTo(900, 190, 925,150);
-        crc2.fillStyle="green";
-        crc2.stroke();        
-        crc2.fill();
-        crc2.closePath();
-        
-        // FELSEN 1
-        crc2.beginPath();
-        crc2.moveTo(200, 550);
-        crc2.lineTo(325, 500);
-        crc2.lineTo(500, 700);
-        crc2.lineTo(200, 700);
-        crc2.lineTo(200, 550);
-        crc2.closePath();
-        crc2.fillStyle="gray";
-        crc2.stroke();
-        crc2.fill();
-        
-        // FELSEN 2
-        crc2.beginPath();
-        crc2.moveTo(400, 550);
-        crc2.lineTo(450, 500);
-        crc2.lineTo(600, 700);
-        crc2.lineTo(200, 700);
-        crc2.lineTo(150, 650);
-        crc2.lineTo(400, 550);
-        crc2.closePath();
-        crc2.fillStyle="gray";
-        crc2.stroke();
-        crc2.fill();
-        
-        // SAND        
-        crc2.beginPath();
-        crc2.moveTo(0, 650);
-        crc2.quadraticCurveTo(350, 500, 500, 600);
-        crc2.quadraticCurveTo(800,500,1000,400); 
-        crc2.lineTo(1000, 700);
-        crc2.lineTo(0, 700);
-        crc2.lineTo(0, 650);
-        crc2.closePath();
-        crc2.fillStyle="lightyellow";
-        crc2.stroke();
-        crc2.fill();
-        
-        
-        
-        // Funktionsaufruf
-        /*drawFish(300, 150);
+        function drawGrass(_x: number, _y: number): void {
+            crc2.beginPath();
+            crc2.moveTo(_x, _y);
+            crc2.quadraticCurveTo(_x - 25, _y + 50, 925, 250); //(900, 200, 925, 250);
+            crc2.quadraticCurveTo(_x + 25, _y + 125, 950, 350); //(950, 275, 950, 350);
+            crc2.quadraticCurveTo(_x, _y + 250, 960, 450); //(925, 400, 960, 450);
+            crc2.lineTo(_x - 35, _y + 300); //(890, 450);
+            crc2.quadraticCurveTo(_x - 25, _y + 250, 920, 350); //(900, 400, 920, 350);
+            crc2.quadraticCurveTo(_x + 15, _y + 150, 900, 250); //(940, 300, 900, 250);
+            crc2.quadraticCurveTo(_x - 25, _y + 40, 925, 150); //(900, 190, 925, 150);
+            crc2.fillStyle = "green";
+            crc2.strokeStyle = "green";
+            crc2.stroke();
+            crc2.fill();
+            crc2.closePath();
+        }
 
-        for (let i: number = 0; i < 10; i++) {
-            let x: number = Math.random() * crc2.canvas.width;
-            let y: number = Math.random() * crc2.canvas.height;
-            drawFish(x, y);
-        }*/
-        
+        // SAND 
+        function drawSand(): void {
+            crc2.beginPath();
+            crc2.moveTo(0, 650);
+            crc2.quadraticCurveTo(350, 500, 500, 600);
+            crc2.quadraticCurveTo(800, 500, 1000, 400);
+            crc2.lineTo(1000, 700);
+            crc2.lineTo(0, 700);
+            crc2.lineTo(0, 650);
+            crc2.closePath();
+            crc2.fillStyle = "lightyellow"; // Füllung
+            crc2.strokeStyle = "lightyellow"; // Linienstyle
+            crc2.stroke();
+            crc2.fill();
+        }
 
+        // BUBBLES 
+        function drawBubbles(_x: number, _y: number, _w: number, _h: number, _a: number): void {
 
-    }// init funktion zu
+            for (let i: number = 0; i < _a; i++) { // for Schleife die Koordinaten für Bubbles festlegt
+                 let width: number = Math.random() * _w;
+                 let height: number = Math.random() * _h;
+                 let scale: number = Math.random() * 13;
 
-   /* function drawFish(_x: number, _y: number): void {
-        crc2.beginPath();
-        crc2.moveTo(_x, _y);
-        crc2.quadraticCurveTo(_cpx + 30, _cpy - 25, _x + 80, _y + 20);
-        crc2.lineTo(_x + 80, _y - 10);
-        crc2.quadraticCurveTo(_cpx + 30, _cpy + 35, _x, _y);
-        crc2.stroke();
-        crc2.closePath();
-    };*/
+            crc2.beginPath();
+            // crc2.arc(150, 150, 30, 60, 3); -> wird in init aufgerufen
+            crc2.arc(_x + width, _y + height, scale, 0, 3 * Math.PI);
+            crc2.stroke();
+            crc2.fillStyle = "darkblue"; // Füllung
+            crc2.strokeStyle = "darkblue"; // Linienstyle
+            crc2.fill();
+            }
+        }
 
+        // FISCHE
+        function drawFish(_x: number, _y: number): void {
+            
+            crc2.beginPath();
+            crc2.moveTo(_x, _y);
+            crc2.quadraticCurveTo(_x + 30, _y - 35, _x + 80, _y + 20);
+            crc2.lineTo(_x + 80, _y - 10);
+            crc2.quadraticCurveTo(_x + 30, _y + 35, _x, _y);
+            crc2.fillStyle = "orange";
+            crc2.strokeStyle = "orange";
+            crc2.stroke();
+            crc2.fill();
+        }
 
-
+        // SCHATZTRUHE
+        function drawBox(_x: number, _y: number): void { // drawBox(620, 600);
+            crc2.beginPath(); //Box von der Seite
+            crc2.moveTo(_x, _y);
+            crc2.lineTo(_x + 200, _y);
+            crc2.lineTo(_x + 200, _y - 100);
+            crc2.lineTo(_x, _y - 100);            
+            crc2.closePath();
+            crc2.fillStyle = "brown";
+            crc2.strokeStyle = "white";
+            crc2.stroke();
+            crc2.fill();
+            
+            crc2.beginPath(); //Deckel von der Seite
+            crc2.moveTo(_x, _y - 100);
+            crc2.lineTo(_x + 190, _y - 150);  
+            crc2.quadraticCurveTo(_x + 70, _y - 190, _x, _y - 100);         
+            crc2.closePath();
+            crc2.fillStyle = "brown";
+            crc2.strokeStyle = "brown";
+            crc2.stroke();
+            crc2.fill();
+        }
 
 
 }// namespace zu
